ec3939ed0963a4a100bb1a5adc60e58db15992e8
@Aadi-Coder <91477251+aadityaagrawal123@users.noreply.github.com>
2024-05-09 22:38:01 +0530

    Add files via upload

---
+++ ar_weather.js
@@ -0,0 +1,44 @@
+let coordinates = {}
+
+$(document).ready(function () {
+    get_coordinates();
+    get_weather();
+})
+
+function get_coordinates() {
+    let searchParams = new URLSearchParams(window.location.search)
+    
+    if (searchParams.has('source') && searchParams.has('destination')) {
+        let source = searchParams.get('source')
+        let destination = searchParams.get('destination')
+        coordinates.source_lat = source.split(";")[0]
+        coordinates.source_lon = source.split(";")[1]
+        coordinates.destination_lat = destination.split(";")[0]
+        coordinates.destination_lon = destination.split(";")[1]
+    } else {
+        alert("Coordinates not selected!")
+        window.history.back();
+    }
+}
+
+function get_weather () {
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.destination_lat}&lon=${coordinates.destination_lon}&appid=6be9fac2c278d09c65cdb4ae67421c26`,
+        type: "get",
+        success: function (response) {
+            let name = response.name;
+            let weather = response.weather[0].main;
+            let temp = `${Math.floor(parseInt(response.main.temp) - 273.15)}\u00B0C`;
+
+            let textEl = `The Weather of ${name} - Weather Type: ${weather}, Temperature: ${temp}`;
+
+            $("#scene_container").append(
+                `
+                <a-entity gps-entity-place="latitude: ${steps[i].maneuver.location[1]}; longitude: ${steps[i].maneuver.location[0]};" >
+                    <a-entity><a-text height="50" value=${textEl}></a-text></a-entity>
+                </a-entity>
+                `
+            );
+        }
+    });
+}
